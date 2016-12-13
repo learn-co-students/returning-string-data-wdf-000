b1e55e92062890e518b5eed91ca770dc7b6f55b6
Flatiron School <github email address>
2016-12-13 13:23:59 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,42 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
 
+  def body
+    # binding.pry
+    post = Post.find(params[:id])
+    render plain: post.description
+    # explicitly render plain text, so we call render with the :plain option.
+  end
+
   def index
     @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @action = "Submit"
     @post = Post.new
+
   end
 
   def create
+    # binding.pry
+
     @post = Post.create(post_params)
     @post.save
     redirect_to post_path(@post)
   end
 
   def edit
+    @action = "Update"
   end
 
+
   def update
+
     @post.update(post_params)
     redirect_to post_path(@post)
   end
